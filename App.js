7fa12b3f88033094db3faf9bb5595307f68196f4
Elgussgood <gsuarezr2000@alumno.ipn.mx>
2022-10-18 16:42:51 -0500

    Ver3

--- App.js
+++ App.js
@@ -3,95 +3,101 @@ import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useState } from 'react';
 import React from 'react';
 const buttons = ['7', '8', '9', '4', '5', '6', '1', '2', '3', 0, 'AC', 'DEL', 'CONVERTIR']
-const options = ['C=>F', 'C=>K', 'F=>C', 'F=>K', 'K=>C','K=>F']
-var x =10;
-var muestra =""
-function teclado (tecla){
+const options = ['C=>F', 'C=>K', 'F=>C', 'F=>K', 'K=>C', 'K=>F']
+var x = 10;
+var muestra = ""
 
-console.log(tecla);
-var y
-if(tecla === 'DEL'||tecla==='AC'||tecla==='CONVERTIR'){
-  console.log("no es numero");
-}else{
-  muestra +=tecla
-  console.log("muestra" +muestra);
-  y = parseFloat(tecla)
-  x += y
-  console.log(x);
-}
-}
+
+
+
 const styles = StyleSheet.create({
   result: {
     backgroundColor: "#4B4B4B",
-    width:"100%",
-    height:120,
-    minHeight: 400, 
-    alignItems:"flex-end",
-    justifyContent:'flex-end'
+    width: "100%",
+    height: 120,
+    minHeight: 400,
+    alignItems: "flex-end",
+    justifyContent: 'flex-end'
   },
-  txtcolor:{
-    color:'#B9BF00',
+  txtcolor: {
+    color: '#B9BF00',
     fontSize: 15
   },
-  resultText:{
-    margin:10, 
-    fontSize:50,
-    color:'#F7FF00'
+  resultText: {
+    margin: 10,
+    fontSize: 50,
+    color: '#F7FF00'
   },
-  button:{  
-    borderColor:'#525900' ,
+  button: {
+    borderColor: '#525900',
     backgroundColor: '#3F3F3F',
-    minWidth:130, 
-    minHeight:96.5,
-    flex:2,
-    alignItems:'center',
-    justifyContent:'center',
-    borderWidth:.5
+    minWidth: 130,
+    minHeight: 96.5,
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: .5
   },
-  buttons:{
-    
+  buttons: {
+
     flexDirection: 'row',
-    flexWrap:'wrap',
+    flexWrap: 'wrap',
 
   },
-  options:{
+  options: {
     flexDirection: 'row',
-    flexWrap:'wrap',
+    flexWrap: 'wrap',
   },
-  opcion:{
-    borderColor:'#525900' ,
+  opcion: {
+    borderColor: '#525900',
     backgroundColor: '#3F3F3F',
-    minWidth:5, 
-    minHeight:65,
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    borderWidth:1
+    minWidth: 5,
+    minHeight: 65,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1
   }
 })
 export default function App() {
-  
+
+  const [res, setRes] = useState("")
+
+  function teclado(tecla) {
+
+    console.log(tecla);
+    var y
+    if (tecla === 'DEL' || tecla === 'AC' || tecla === 'CONVERTIR') {
+      console.log("no es numero");
+    } else {
+      muestra += tecla
+      console.log("muestra" + muestra);
+      y = parseFloat(tecla)
+      x += y
+      console.log(x);
+    }
+    setRes(muestra)
+  }
+
   return (
     <View>
       <View style={styles.result}>
-        <Text style={styles.resultText}>{tecla}</Text>
+        <Text style={styles.resultText}>{res}</Text>
       </View>
       <View style={styles.options}>
-        {options.map((opcion)=>
-        <TouchableOpacity key={opcion} style={styles.opcion}>
-          <Text style={styles.txtcolor}>{opcion}</Text>
-        </TouchableOpacity>
+        {options.map((opcion) =>
+          <TouchableOpacity key={opcion} style={styles.opcion}>
+            <Text style={styles.txtcolor}>{opcion}</Text>
+          </TouchableOpacity>
         )}
       </View>
       <View style={styles.buttons}>
-        {buttons.map((button)=>
-          <TouchableOpacity key={button} style={styles.button} onPress={()=> teclado(button)}>
+        {buttons.map((button) =>
+          <TouchableOpacity key={button} style={styles.button} onPress={() => teclado(button)}>
             <Text style={styles.txtcolor}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
     </View>
   );
-}
-
-;
+};
